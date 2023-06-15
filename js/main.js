146b75afc20e78c9997b45b601668894a57de914
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2023-06-15 11:49:37 -0300

    Colocando efeito no paragrafo da seção principal.

--- js/main.js
+++ js/main.js
@@ -6,3 +6,18 @@ function toggleMenu() {
 }
 
 btnMobile.addEventListener("click", toggleMenu);
+
+
+const descript = document.querySelector('.descript');
+
+function typeWrite(element){
+  const textoArray = element.innerHTML.split('');
+  element.innerHTML = '';
+  textoArray.forEach((letra, i) =>{
+    setTimeout(function() {
+      element.innerHTML += letra;
+    }, 55 * i)
+  })
+}
+
+typeWrite(descript)
