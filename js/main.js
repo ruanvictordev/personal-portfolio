63f5354045387d283d6f6a08485cd3059517a9d7
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2023-07-23 17:50:08 -0300

    Adicionando page do projeto ProWayComputers.

--- js/main.js
+++ js/main.js
@@ -29,29 +29,6 @@ function typeWrite(element){
 typeWrite(descript)
 
 
-// ARROW UP PAGE EFECT ======================================================================
-
-const btnTop = document.getElementById("btnTop");
-
-btnTop.addEventListener("click", function(){
-  window.scrollTo(0,0)
-})
-
-// Ocultar
-
-document.addEventListener('scroll', ocultar)
-
-function ocultar(){
-  if(window.scrollY > 10){
-    btnTop.style.display = 'flex';
-  } else {
-    btnTop.style.display = 'none';
-  }
-}
-
-ocultar()
-
-
 // SCROLL MENU EFECT =============================================================================
 
 
